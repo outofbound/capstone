ee59ce897d767316662f46ed6bafdcb31b84c1cb
jessicayu <non_hill@hotmail.com>
2014-02-18 00:22:47 -0500

    what

--- KinectHoverButton.cs
+++ KinectHoverButton.cs
@@ -3,7 +3,7 @@
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
 //------------------------------------------------------------------------------
-kljk
+
 namespace Microsoft.Samples.Kinect.ControlsBasics
 {
     using System;
